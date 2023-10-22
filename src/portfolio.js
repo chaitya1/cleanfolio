2d26a5ace31e3b7e48e1811a2656974bed92c669
Chaitya L <60097116+chaitya1@users.noreply.github.com>
2023-10-22 18:39:35 -0400

    Update portfolio.js
    PM Details

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,13 +1,13 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://chaitya1.github.io/cleanfolio',
   title: 'JS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Chaitya Lodaya',
+  role: 'Project Manager',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
@@ -49,23 +49,19 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Project Management',
+  'Agile',
+  'Jira',
+  'Microsft Office',
+  'Stakeholder Management',
+  'Risk Assessment',
+  'Client Relationship Management',
+  ,
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'chaityalodaya@mail.com',
 }
 
 export { header, about, projects, skills, contact }
